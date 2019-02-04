a2cf1ccd7c043ccf44080e6ba09094179cdb61c9
Maddy Haldeman <madelyn.haldeman@gmail.com>
2019-02-04 15:36:26 -0800

    debug JS

--- js/scripts.js
+++ js/scripts.js
@@ -3,108 +3,124 @@
 //pizza constructor 
 function Pizza(pSize, pSauce) {
   // this.id = id;
-  this.pSize = pSize;
-  this.pSauce = pSauce; //all = 1
-  this.cheese = 0; //all = .50
-  this.meat = 0; //all = 1.00
-  this.topping = 0; //all = .25
-  this.price = 0
+  this.pSize = pSize,
+  this.pSauce = pSauce, 
+  this.cheeses = [],
+  this.meats = [],
+  this.toppings = [],
+  this.price = 0,
+  this.saucePrice = 0,
+  this.meatPrice = 0,
+  this.cheesePrice = 0,
+  this.toppingPrice = 0
 }
 
 // prototypes 
-Pizza.prototype.sizePrice = function() {
-  if (this.pSize === "Small" && this.cheese) {
-    this.price += 3.00;
+Pizza.prototype.addCheese = function(cheese) {
+  this.cheeses.push(cheese);
+}
+
+Pizza.prototype.addMeat = function(meat) {
+  this.meats.push(meat);
+}
+
+Pizza.prototype.addTopping = function(topping) {
+  this.toppings.push(topping);
+}
+
+Pizza.prototype.sizeCost = function() {
+  if (this.pSize === "Small") {
+    this.price = 3.00;
   } else if (this.pSize === "Medium") {
-    this.price += 5.00;
+    baseprice = 5.00;
   } else if (this.pSize === "Large") {
-    this.price += 8.00;
-  } else {
-    this.price += 10.00;
-  }
+    this.price = 8.00;
+  } else if (this.pSize === "XLarge") {
+    this.price = 10.00;
+  };
+  return this.price;
 }
 
-Pizza.prototype.saucePrice = function() {
+Pizza.prototype.sauceCost = function() {
   if (this.pSauce === "Pesto" || this.pSauce === "Olive" || this.pSauce === "Red") {
-    this.price += 1.00;
+    this.saucePrice = 1.00;
   }
 }
 
-Pizza.prototype.cheesePrice = function() {
+Pizza.prototype.cheeseCost = function() {
   if(document.getElementById("ricotta").checked) {
-    this.cheese += 1;
-    this.price += this.cheese;
-  } if (document.getElementById("mozzarella").checked) {
-    this.cheese += 1;
-    this.price += this.cheese;
+    this.cheesePrice += 1;
+  } if(document.getElementById("mozzarella").checked) {
+    this.cheesePrice += 1;
   } if(document.getElementById("parmesan").checked) {
-    this.cheese += 1;
-    this.price += this.cheese;
+    this.cheesePrice += 1;
   }
 }
 
-Pizza.prototype.meatPrice = function() {
+Pizza.prototype.meatCost = function() {
   if(document.getElementById("sausage").checked) {
-    this.meat += 1;
-    this.price += this.meat;
+    this.meatPrice += 1;
   } if (document.getElementById("prosciutto").checked) {
-    this.meat += 1.50;
-    this.price += this.meat;
+    this.meatPrice += 1.50;
   } if(document.getElementById("chicken").checked) {
-    this.meat += 1;
-    this.price += this.meat;
+    this.meatPrice += 1;
   }
 }
 
-Pizza.prototype.toppingPrice = function() {
+Pizza.prototype.toppingCost = function() {
   if(document.getElementById("spinach").checked) {
-    this.topping += .50;
-    this.price += this.topping;
+    this.toppingPrice += .50;
   } if (document.getElementById("artichoke").checked) {
-    this.topping += 1.00;
-    this.price += this.topping;
+    this.toppingPrice += 1;
   } if(document.getElementById("olives").checked) {
-    this.topping += 1;
-    this.price += this.topping;
+    this.toppingPrice += 1;
   }
+}
+
+Pizza.prototype.totalCost = function() {
+  return this.price += this.saucePrice += this.cheesePrice += this.meatPrice += this.toppingPrice;
 }
 
 //user interface logic
-
+var newPizza = new Pizza();
 $(document).ready(function() {
   $("form#formOne").submit(function() {
     event.preventDefault();
 
     var pSize = $("#pSize").val();
     var pSauce = $("#pSauce").val();
-    cheese = [];
-    meat = [];
-    topping = [];
+    var pCheese = [];
+    var pMeat = [];
+    var pTopping = [];
 
     $("input:checkbox[name=cheese]:checked").each(function() {
-      cheese.push($(this).val());
+      pCheese.push($(this).val());
     });
 
     $("input:checkbox[name=meat]:checked").each(function() {
-      meat.push($(this).val());
+      pMeat.push($(this).val());
     });
 
     $("input:checkbox[name=pTopping]:checked").each(function() {
-      topping.push($(this).val());
+      pTopping.push($(this).val());
     });
 
-    var newPizza = new Pizza(pSize, pSauce, cheese, meat, topping);
+    var newPizza = new Pizza(pSize, pSauce, pCheese, pMeat, pTopping);
+    newPizza.addCheese(pCheese);
+    newPizza.addMeat(pMeat);
+    newPizza.addTopping(pTopping)
+
+    newPizza.sizeCost();
+    newPizza.sauceCost();
+    newPizza.cheeseCost();
+    newPizza.meatCost();
+    newPizza.toppingCost();
+    newPizza.totalCost();
     
-    newPizza.sizePrice();
-    newPizza.saucePrice();
-    newPizza.cheesePrice();
-    newPizza.meatPrice();
-    newPizza.toppingPrice();
-    
-    $('.total').text("Thank you for your order, your total is: " + newPizza.price.toFixed(2));
+    $('.total').text("Thank you for your order, your total is: " + newPizza.totalCost().toFixed(2));
 
 
-  });
+  })
 
-});
+})
 
